0170c511a91c31df8397fb0f2f612b56870906e0
javed432 <javed.akhtar432@gmail.com>
2020-04-11 03:44:42 +0530

    Fonts, Screens and React Navigaton files added

--- App.js
+++ App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const FetchFonts = () => {
+  return Font.loadAsync({
+    "circular-std": require("./assets/fonts/CircularStd-Book.ttf"),
+    "circular-std-bold": require("./assets/fonts/CircularStd-Medium.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={FetchFonts}
+        onFinish={() => setFontLoaded(true)}
+      />
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.txt}>
+        Open up App.js to start working on your app
+      </Text>
     </View>
   );
 }
@@ -12,8 +34,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  txt: {
+    fontFamily: "circular-std",
+    fontSize: 16,
   },
 });
